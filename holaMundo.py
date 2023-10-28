78ed3cf2ad48935101714a8cff9194b8b9e8b305
Jhon-clase1 <jhoncensa2023@gmail.com>
2023-10-28 12:03:39 -0500

    Calcular el area de un cuadrado

---
+++ holaMundo.py
@@ -0,0 +1,12 @@
+number1 = 5
+texto = 'texto'
+boolean= False
+float = 5.6 
+sum = number1 + float
+#Calcular el area de un cuadrado
+base = int(input('ingrese la base: '))
+altura = int(input('ingrese la altura: '))
+
+resultado = base * altura
+
+print(resultado)
